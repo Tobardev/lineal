27d8ba28e0c2a336e83854d5cecf9fe4c4128550
Juan Camilo Quiceno <juan.camilo.quiceno@correounivalle.edu.co>
2025-12-09 15:54:56 -0500

    Agregar cálculo de distancia 3D para landmarks

--- geometryutils.py
+++ geometryutils.py
@@ -22,3 +22,17 @@ def distancia2(a, b):
         float: Distancia 2D entre los puntos
     """
     return math.hypot(a.x - b.x, a.y - b.y)
+def distancia3(a, b):
+    """
+    Calcula la distancia Euclidiana 3D entre dos landmarks
+    Args:
+        a, b: Landmarks con atributos .x, .y, .z
+    Returns:
+        Float: Distancia 3D entre los puntos
+    """
+dx = a.x - b.x
+dy = a.y - b.y
+dz = a.z - b.z
+
+return math.sqrt(dx*dx + dy*dy + dz*dz)
+
